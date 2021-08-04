e03f1b16db94c76bcbacee7dedf4cf14027c739d
zhongkai01@corp.netease.com <zhongkai01@corp.netease.com>
2021-08-04 10:43:25 +0800

    删除windows文件夹。windows使用qt或者 electron代替

--- windows/build_win32.bat
+++
@@ -1,9 +0,0 @@
-cd %~dp0
-
-chcp 65001
-mkdir bin
-mkdir bin\NEMWhiteboard
-
-xcopy whiteboard-sample\nim_sdk\bin bin /s
-copy whiteboard-plugin\NEMWhiteboard.qml bin\NEMWhiteboard
-copy whiteboard-plugin\qmldir bin\NEMWhiteboard
--- windows/whiteboard-plugin/whiteboard_plugin.cpp
+++
@@ -1,7 +0,0 @@
-#include "whiteboard_plugin.h"
-#include <qqml.h>
-#include "nem_jsBridge.h"
-
-void NEMWhiteboardPlugin::registerTypes(const char* uri) {
-    qmlRegisterType<NEMJsBridge>(uri, 1, 0, "NEMJsBridge");
-}
